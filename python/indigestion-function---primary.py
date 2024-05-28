b386dfde33f7b71686357b2e74689e45fb6033c6
Martin Chapman <contact@martinchapman.co.uk>
2024-05-28 12:16:55 +0100

    Update made by Phenoflow

---
+++ python/indigestion-function---primary.py
@@ -0,0 +1,25 @@
+# Rebecca M Joseph, Ruth H Jack, Richard Morriss, Roger David Knaggs, Debbie Buttler, Chris Hollis, Julia Hippisley-Cox, Carrol Coupland, 2024.
+
+import sys, csv, re
+
+codes = [{"code":"J16yA11","system":"readv2"},{"code":"J16yz00","system":"readv2"},{"code":"J16z.00","system":"readv2"},{"code":"J16y500","system":"readv2"}];
+REQUIRED_CODES = 1;
+with open(sys.argv[1], 'r') as file_in, open('indigestion-potential-cases.csv', 'w', newline='') as file_out:
+    csv_reader = csv.DictReader(file_in)
+    csv_writer = csv.DictWriter(file_out, csv_reader.fieldnames + ["indigestion-function---primary-identified"])
+    csv_writer.writeheader();
+    codes_identified = 0;
+    for row in csv_reader:
+        newRow = row.copy();
+        for cell in row:
+            # Iterate cell lists (e.g. codes)
+            for item in re.findall(r'\(([^,]*)\,', row[cell]):
+                if(item in list(map(lambda code: code['code'], codes))): codes_identified+=1;
+                if(codes_identified>=REQUIRED_CODES):
+                    newRow["indigestion-function---primary-identified"] = "CASE";
+                    break;
+            if(codes_identified>=REQUIRED_CODES): break;
+        if(codes_identified<REQUIRED_CODES):
+            newRow["indigestion-function---primary-identified"] = "UNK";
+        codes_identified=0;
+        csv_writer.writerow(newRow)
